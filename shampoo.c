a576c8e116d12d56f9c7d493d5dc6080dc6cc43a
Billal-Hossen <mddipu867@gmail.com>
2024-06-03 12:19:11 +0600

    write a c program for get a fibonacci series based on size of n

---
+++ shampoo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int v, a,b,c;
+scanf("%d %d %d %d",&v,&a,&b,&c);
+
+int sum = a+b+c;
+float valuse = double v/ double sum;
+printf("%d %d %d %d %f\n",v,a,b,c,valuse);
+//printf("loop=%f",loop);
+return 0;
+
+}
